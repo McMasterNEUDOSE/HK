e5ab9cc6ec2af3fe6cf2273fb7ca67eea6438cce
PaulaBosca <paula.bosca@outlook.com>
2020-06-23 16:23:56 -0400

    Fixed table generation

--- fsw/src/hk_app.h
+++ fsw/src/hk_app.h
@@ -2,41 +2,41 @@
 ** File:
 **   $Id: hk_app.h 1.2 2015/11/10 16:49:05EST lwalling Exp  $
 **
-**  Copyright © 2007-2014 United States Government as represented by the 
-**  Administrator of the National Aeronautics and Space Administration. 
-**  All Other Rights Reserved.  
+**  Copyright © 2007-2014 United States Government as represented by the
+**  Administrator of the National Aeronautics and Space Administration.
+**  All Other Rights Reserved.
 **
 **  This software was created at NASA's Goddard Space Flight Center.
-**  This software is governed by the NASA Open Source Agreement and may be 
-**  used, distributed and modified only pursuant to the terms of that 
+**  This software is governed by the NASA Open Source Agreement and may be
+**  used, distributed and modified only pursuant to the terms of that
 **  agreement.
 **
-** Purpose: 
+** Purpose:
 **  The CFS Housekeeping (HK) Application header file
 **
 ** Notes:
 **
 ** $Log: hk_app.h  $
-** Revision 1.2 2015/11/10 16:49:05EST lwalling 
+** Revision 1.2 2015/11/10 16:49:05EST lwalling
 ** Restore data lost in MKS 2010 from MKS 2009
-** Revision 1.1 2015/07/25 21:31:31EDT rperera 
+** Revision 1.1 2015/07/25 21:31:31EDT rperera
 ** Initial revision
 ** Member added to project /CFS-APPs-PROJECT/hk/fsw/src/project.pj
-** Revision 1.8 2015/03/04 14:58:30EST sstrege 
+** Revision 1.8 2015/03/04 14:58:30EST sstrege
 ** Added copyright information
-** Revision 1.7 2009/12/03 17:00:17EST jmdagost 
+** Revision 1.7 2009/12/03 17:00:17EST jmdagost
 ** Added include of hk_msgdefs.h
-** Revision 1.6 2009/04/18 13:02:36EDT dkobe 
+** Revision 1.6 2009/04/18 13:02:36EDT dkobe
 ** Corrected doxygen comments
-** Revision 1.5 2008/09/11 11:41:35EDT rjmcgraw 
+** Revision 1.5 2008/09/11 11:41:35EDT rjmcgraw
 ** DCR 4041:1 Added #include hk_platform_cfg.h
-** Revision 1.4 2008/07/15 16:52:49EDT rjmcgraw 
+** Revision 1.4 2008/07/15 16:52:49EDT rjmcgraw
 ** DCR4042:1 Removed old #define HK_REV_NUM
-** Revision 1.3 2008/06/19 13:54:39EDT rjmcgraw 
+** Revision 1.3 2008/06/19 13:54:39EDT rjmcgraw
 ** DCR3052:1 Table processing fix - changed version number from 0.1 to 0.2
-** Revision 1.2 2008/05/02 12:18:13EDT rjmcgraw 
+** Revision 1.2 2008/05/02 12:18:13EDT rjmcgraw
 ** DCR1647:1 Moved performance marker to hk_perfids.h
-** Revision 1.1 2008/04/09 16:41:46EDT rjmcgraw 
+** Revision 1.1 2008/04/09 16:41:46EDT rjmcgraw
 ** Initial revision
 ** Member added to CFS project
 **
@@ -68,7 +68,7 @@
 /************************************************************************
 ** Type Definitions
 *************************************************************************/
-/** 
+/**
 **  \brief HK global data structure
 */
 typedef struct
@@ -83,23 +83,23 @@ typedef struct
     */
     CFE_SB_MsgPtr_t         MsgPtr;/**< \brief Pointer to msg received on software bus */
 
-    CFE_SB_PipeId_t         CmdPipe;/**< \brief Pipe Id for HK command pipe */    
+    CFE_SB_PipeId_t         CmdPipe;/**< \brief Pipe Id for HK command pipe */
     uint8					CmdCounter;/**< \brief Number of valid commands received */
     uint8					ErrCounter;/**< \brief Number of invalid commands received */
     uint8					Spare;/**< \brief Spare byte for alignment */
 
     uint16					MissingDataCtr;/**< \brief Number of times missing data was detected */
-    uint16					CombinedPacketsSent;/**< \brief Count of combined output msgs sent */    
+    uint16					CombinedPacketsSent;/**< \brief Count of combined output msgs sent */
 
-    uint32                  MemPoolHandle;/**< \brief HK mempool handle for output pkts */
+    CFE_ES_MemHandle_t      MemPoolHandle;/**< \brief HK mempool handle for output pkts */
     uint32                  RunStatus;/**< \brief HK App run status */
-        
+
     CFE_TBL_Handle_t        CopyTableHandle;/**< \brief Copy Table handle */
     CFE_TBL_Handle_t        RuntimeTableHandle;/**< \brief Run-time table handle */
 
     hk_copy_table_entry_t   *CopyTablePtr;/**< \brief Ptr to copy table entry */
     hk_runtime_tbl_entry_t  *RuntimeTablePtr;/**< \brief Ptr to run-time table entry */
-        
+
     uint8                   MemPoolBuffer [HK_NUM_BYTES_IN_MEM_POOL];/**< \brief HK mempool buffer */
 
 } HK_AppData_t;
@@ -115,13 +115,13 @@ extern HK_AppData_t             HK_AppData;/**< \brief HK Housekeeping Packet */
 *************************************************************************/
 /************************************************************************/
 /** \brief CFS Housekeeping (HK) application entry point
-**  
+**
 **  \par Description
 **       Housekeeping application entry point and main process loop.
 **
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 *************************************************************************/
 void HK_AppMain(void);
 
@@ -131,16 +131,16 @@ void HK_AppMain(void);
 *************************************************************************/
 /************************************************************************/
 /** \brief Initialize the housekeeping application
-**  
+**
 **  \par Description
-**       Housekeeping application initialization routine. This 
-**       function performs all the required startup steps to 
+**       Housekeeping application initialization routine. This
+**       function performs all the required startup steps to
 **       get the application registered with the cFE services so
-**       it can begin to receive command messages. 
+**       it can begin to receive command messages.
 **
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 **  \returns
 **  \retcode #CFE_SUCCESS  \retdesc \copydoc CFE_SUCCESS \endcode
 **  \retstmt Return codes from #CFE_EVS_Register         \endcode
@@ -154,14 +154,14 @@ int32 HK_AppInit (void);
 
 /************************************************************************/
 /** \brief Initialize the Copy Table and the Runtime Table
-**  
+**
 **  \par Description
-**       Registers the Copy table and Runtime table with cFE Table 
-**       Services. Also processes the copy table. 
+**       Registers the Copy table and Runtime table with cFE Table
+**       Services. Also processes the copy table.
 **
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 **  \sa #HK_AppInit
 **
 *************************************************************************/
@@ -170,17 +170,17 @@ int32 HK_TableInit (void);
 
 /************************************************************************/
 /** \brief Process a command pipe message
-**  
+**
 **  \par Description
 **       Processes a single software bus command pipe message. Checks
 **       the message and command IDs and calls the appropriate routine
 **       to handle the command.
-**       
+**
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 **  \param [in]  MessagePtr   A #CFE_SB_MsgPtr_t pointer that
-**                            references the software bus message 
+**                            references the software bus message
 **
 **  \sa #CFE_SB_RcvMsg
 **
@@ -190,17 +190,17 @@ void HK_AppPipe (CFE_SB_MsgPtr_t MessagePtr);
 
 /************************************************************************/
 /** \brief Send Combined Housekeeping Message
-**  
+**
 **  \par Description
 **       Processes the command to send a combined housekeeping message
 **
 **  \par Assumptions, External Events, and Notes:
-**       This command does not affect the command execution counter, but 
+**       This command does not affect the command execution counter, but
 **       this command will increment the cmd error counter if an invalid cmd
 **       length is detected.
 **
 **  \param [in]  MessagePtr   A #CFE_SB_MsgPtr_t pointer that
-**                            references the software bus message 
+**                            references the software bus message
 **
 *************************************************************************/
 void HK_SendCombinedHKCmd(CFE_SB_MsgPtr_t MessagePtr);
@@ -208,17 +208,17 @@ void HK_SendCombinedHKCmd(CFE_SB_MsgPtr_t MessagePtr);
 
 /************************************************************************/
 /** \brief Process housekeeping request
-**  
+**
 **  \par Description
 **       Processes an on-board housekeeping request message.
 **
 **  \par Assumptions, External Events, and Notes:
-**       This command does not affect the command execution counter, but 
+**       This command does not affect the command execution counter, but
 **       this command will increment the cmd error counter if an invalid cmd
 **       length is detected.
 **
 **  \param [in]  MessagePtr   A #CFE_SB_MsgPtr_t pointer that
-**                            references the software bus message 
+**                            references the software bus message
 **
 *************************************************************************/
 void HK_HousekeepingCmd (CFE_SB_MsgPtr_t MessagePtr);
@@ -226,15 +226,15 @@ void HK_HousekeepingCmd (CFE_SB_MsgPtr_t MessagePtr);
 
 /************************************************************************/
 /** \brief Process noop command
-**  
+**
 **  \par Description
 **       Processes a noop ground command.
 **
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 **  \param [in]  MessagePtr   A #CFE_SB_MsgPtr_t pointer that
-**                            references the software bus message 
+**                            references the software bus message
 **
 **  \sa #HK_NOOP_CC
 **
@@ -244,7 +244,7 @@ void HK_NoopCmd (CFE_SB_MsgPtr_t MessagePtr);
 
 /************************************************************************/
 /** \brief Process reset counters command
-**  
+**
 **  \par Description
 **       Processes a reset counters ground command which will reset
 **       the memory manager commmand error and command execution counters
@@ -252,9 +252,9 @@ void HK_NoopCmd (CFE_SB_MsgPtr_t MessagePtr);
 **
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 **  \param [in]   MessagePtr   A #CFE_SB_MsgPtr_t pointer that
-**                             references the software bus message 
+**                             references the software bus message
 **
 **  \sa #HK_RESET_CC
 **
@@ -264,14 +264,14 @@ void HK_ResetCtrsCmd (CFE_SB_MsgPtr_t MessagePtr);
 
 /************************************************************************/
 /** \brief Reset housekeeping data
-**  
+**
 **  \par Description
 **       Function called in response to a Reset Counters Command. This
 **       function will reset the HK housekeeping data.
 **
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 **  \sa #HK_RESET_CC
 **
 *************************************************************************/
@@ -280,15 +280,15 @@ void HK_ResetHkData (void);
 
 /************************************************************************/
 /** \brief  Verify length of HK commands
-**  
+**
 **  \par Description
-**       Function called when an HK command is received. 
+**       Function called when an HK command is received.
 **
 **  \par Assumptions, External Events, and Notes:
 **       None
-**       
+**
 **  \param [in]   MessagePtr   A #CFE_SB_MsgPtr_t pointer that
-**                             references the software bus message 
+**                             references the software bus message
 **  \param [in]   ExpectedLength   The expected lenght of the command
 **
 **  \returns
@@ -296,7 +296,7 @@ void HK_ResetHkData (void);
 **  \retcode #HK_BAD_MSG_LENGTH_RC if actual cmd length is not as expected
 **  \endreturns
 **
-**  \sa 
+**  \sa
 **
 *************************************************************************/
 int32 HK_VerifyCmdLength (CFE_SB_MsgPtr_t MessagePtr,uint32 ExpectedLength);
